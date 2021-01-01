f9a732d1111965934f3f7c1e658aa6a9236c2821
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-01 19:07:14 +0100

    PlaceController

--- src/main/java/pl/warkoczewski/foodspot/FoodSpotApi.java
+++ src/main/java/pl/warkoczewski/foodspot/FoodSpotApi.java
@@ -1,9 +1,7 @@
 package pl.warkoczewski.foodspot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class FoodSpotApi {
     @GetMapping
     public String sayHello(){
--- src/main/java/pl/warkoczewski/foodspot/controller/PlaceController.java
+++ src/main/java/pl/warkoczewski/foodspot/controller/PlaceController.java
@@ -1,20 +1,23 @@
 package pl.warkoczewski.foodspot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.warkoczewski.foodspot.service.PlaceService;
+
+import pl.warkoczewski.foodspot.service.PlaceServiceImpl;
 
 @Controller
 @RequestMapping("/place")
 public class PlaceController {
-    private final PlaceService placeService;
+    private final PlaceServiceImpl placeService;
 
-    public PlaceController(PlaceService placeService) {
+    public PlaceController(PlaceServiceImpl placeService) {
         this.placeService = placeService;
     }
+
     @GetMapping
-    public String displayGetPlacesPage(){
+    public String displayGetPlacesPage(Model model){
         return "/place/places";
     }
 }
