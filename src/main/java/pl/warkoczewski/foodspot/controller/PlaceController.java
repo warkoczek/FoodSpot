b1ef6363880157f97b3ffdde03d3df30a6aa08fd
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-02 10:02:36 +0100

    place.html form

--- src/main/java/pl/warkoczewski/foodspot/controller/PlaceController.java
+++ src/main/java/pl/warkoczewski/foodspot/controller/PlaceController.java
@@ -3,7 +3,11 @@ package pl.warkoczewski.foodspot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.warkoczewski.foodspot.dto.PlaceQueryDTO;
 import pl.warkoczewski.foodspot.service.PlaceServiceImpl;
 
 @Controller
@@ -15,8 +19,14 @@ public class PlaceController {
         this.placeService = placeService;
     }
 
-    @GetMapping
+    @GetMapping("/places")
     public String displayGetPlacesPage(Model model){
+        model.addAttribute("placeQueryDTO", new PlaceQueryDTO());
         return "/place/places";
     }
+    @PostMapping("/places")
+    public String processPlaceQueryForm(@ModelAttribute PlaceQueryDTO placeQueryDTO){
+        return "/place/places";
+    }
+
 }
