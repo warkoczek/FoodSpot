21732040c178ee69943c2f3558b6cce2f2e6926c
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-03 15:23:25 +0100

    trying to fix why boolean field in DisplayPlaceDTO return nullpointerexception.

--- src/main/java/pl/warkoczewski/foodspot/dto/DisplayPlaceDTO.java
+++ src/main/java/pl/warkoczewski/foodspot/dto/DisplayPlaceDTO.java
@@ -2,6 +2,7 @@ package pl.warkoczewski.foodspot.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 @AllArgsConstructor
 @Data
 public class DisplayPlaceDTO {
--- src/main/java/pl/warkoczewski/foodspot/fetcher/PlaceFetcherImpl.java
+++ src/main/java/pl/warkoczewski/foodspot/fetcher/PlaceFetcherImpl.java
@@ -37,23 +37,6 @@ public class PlaceFetcherImpl implements PlaceFetcher {
         }
         return uri;
     }
-    /*
-    private String buildUri(PlaceQuery placeQuery){
-        return UriComponentsBuilder.fromHttpUrl(BASE_PLACE_SEARCH_GOOGLE_API_URL)
-                .queryParams(addQueryParams(placeQuery)).toUriString();
-    }
 
-    private MultiValueMap<String, String> addQueryParams(PlaceQuery placeQuery) {
-        MultiValueMap<String, String> queryParam = new LinkedMultiValueMap<>();
-        queryParam.add("lat", placeQuery.getLocation().getLat());
-        queryParam.add("lon", placeQuery.getLocation().getLon());
-        queryParam.add("radius", String.valueOf(placeQuery.getRadius()));
-        queryParam.add("type", String.valueOf(placeQuery.getPlace_type()));
-        queryParam.add("keyword", placeQuery.getKeyword());
-        queryParam.add("key",GOOGLE_APIS_PLACE_SEARCH_KEY);
-        return queryParam;
-    }
-
-     */
 
 }
