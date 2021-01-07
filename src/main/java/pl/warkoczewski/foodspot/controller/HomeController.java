19fdd26eefd9658aff34ea11b2fc609fe00edaf3
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-01-07 18:05:30 +0100

    HomeController

--- src/main/java/pl/warkoczewski/foodspot/controller/HomeController.java
+++ src/main/java/pl/warkoczewski/foodspot/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping("/homePage")
     public String displayHomePage(){
-        return "/home/homePage";
+        return "home/homePage";
     }
 }
